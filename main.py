1d22e4933366ea915239f88f92b648db0ad8dd8c
sambux1 <sambuxbaum01@gmail.com>
2023-04-14 12:27:57 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# get input
+# should probably use python bytes data type - I think that's what other python hashes use
+
+# make the bipartite expander graph
+# this is the hardest part
+
+# make the hash from the generated substrings

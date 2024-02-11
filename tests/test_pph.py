6381f78600b1118cde6ac849da4f89d4c2c1fdc5
sambux1 <sambuxbaum01@gmail.com>
2024-02-10 22:23:59 -0500

    unit test for sparsification

--- tests/test_pph.py
+++ tests/test_pph.py
@@ -2,19 +2,34 @@ from unittest import TestCase
 import numpy as np
 import random
 
-
-#TODO: this is messy, use modules
 import sys
 sys.path.append("") # this adds the project parent directory to path
-sys.path.append("src/pph/")
 
-from nonrobust import NonRobust
+#from nonrobust import NonRobust
+import perceptographic
+
 
 class TestPPH(TestCase):
 
     def setUp(self):
         pass
     
+    def test_blv19_sparsification(self):
+        k_vals = [4, 6, 8, 12, 16]
+        sizes = [256, 512, 1024, 2048]
+        for size in sizes:
+            for k in k_vals:
+                for i in range(100):
+                    x1 = np.random.randint(0, 2, size=(size))
+                    x2 = np.random.randint(0, 2, size=(size))
+                    xdiff = np.logical_xor(x1, x2).sum()
+                    y1 = perceptographic.pph.BLV19.sparsify(x1, k)
+                    y2 = perceptographic.pph.BLV19.sparsify(x2, k)
+                    ydiff = np.logical_xor(y1, y2).sum()
+                    self.assertTrue(ydiff >= (2 * xdiff.sum() / k), "Sparsified vector is too sparse.")
+                    self.assertTrue(ydiff.sum() <= (2 * xdiff.sum()), "Sparsified vector is not sparse enough.")
+
+    '''
     def test_correctness(self):
         #TODO: this is pretty badly broken right now
         pph = NonRobust(2048, 256, 200, 0.25)
@@ -48,3 +63,4 @@ class TestPPH(TestCase):
             res = pph.evaluate(y1, y2)
             count_false += 1 if res is False else 0
         self.assertTrue(count_false > 950, count_false)
+    '''
